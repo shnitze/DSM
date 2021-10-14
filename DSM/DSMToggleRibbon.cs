87a93358edaad71703a430677a6d96ef33f69e35
Jonathan Arsenault <jonathan.arsenau@gmail.com>
2021-10-14 14:04:05 -0400

    add explorer ribbon

---
+++ DSM/DSMToggleRibbon.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSM
+{
+    public partial class DSMToggleRibbon
+    {
+        private void DSMToggleRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
