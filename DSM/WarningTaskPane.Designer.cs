63057b02b0114568cbac639b739923404ea4d935
jonathan.arsenault <jonathan.arsenault@hrsdc-rhdcc.gc.ca>
2023-05-18 13:21:22 -0400

    fix banner scaling issue

--- DSM/WarningTaskPane.Designer.cs
+++ DSM/WarningTaskPane.Designer.cs
@@ -31,6 +31,8 @@ namespace DSM
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WarningUserControl));
             this.lblWarningMessage = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblWarningMessage
@@ -39,21 +41,29 @@ namespace DSM
             this.lblWarningMessage.ForeColor = System.Drawing.Color.Black;
             this.lblWarningMessage.Name = "lblWarningMessage";
             // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.lblWarningMessage, 0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
             // WarningUserControl
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.SystemColors.Info;
-            this.Controls.Add(this.lblWarningMessage);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "WarningUserControl";
             this.Load += new System.EventHandler(this.WarningUserControl_Load);
             this.Resize += new System.EventHandler(this.WarningTaskPane_SizeChanged);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Label lblWarningMessage;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
