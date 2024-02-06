03ba5a4ecdcfc41948565a7012a59b853ed29722
jonathan.arsenault <jonathan.arsenault@hrsdc-rhdcc.gc.ca>
2024-02-06 15:11:49 -0500

    update metadata/increment versions

--- DSM/Properties/AssemblyInfo.cs
+++ DSM/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Security;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DSM")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Delay Send Message Outlook Add-in")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SDS-SCS")]
 [assembly: AssemblyProduct("DSM")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2021-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.1")]
 
